e92d02e7f8f763acdd9a1264178cca0862c62e5c
Usuario <Usuario@CPD02-M02>
2024-04-30 11:04:08 -0300

    corrigido

--- ConsoleAppArrayMes/Program.cs
+++ ConsoleAppArrayMes/Program.cs
@@ -10,12 +10,12 @@ namespace ConsoleAppArrayMes
     {
         static void Main(string[] args)
         {
-            string[] meses = { "", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+            string[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
 
             Console.WriteLine("Digite o número de um mês de 1 a 12");
             int i = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("O mês escolhido foi " + meses[i]);
+            Console.WriteLine("O mês escolhido foi " + meses[i-1]);
             Console.ReadKey();
         }
     }
